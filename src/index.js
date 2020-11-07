2b78d4562b496b35d7cd7a7355689e227c127717
sayeed38 <mohammedsayeed1997@gmail.com>
2020-11-07 18:10:02 +0530

    Modified Routing

--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@ const httpServer = http.createServer(handleServer);
 
 function handleServer(req, res) {
   if(req.url === '/welcome'){
-      res.write("Welcome to Dominos!");
+      res.send("Welcome to Dominos!");
       res.end();
   }
   if(req.url === '/contact'){
-      res.write({
-        phone: '18602100000',
-        email: 'guestcaredominos@jublfood.com'
+      res.send({
+        "phone": '18602100000',
+        "email": 'guestcaredominos@jublfood.com'
         });
         res.end();
   }else{
